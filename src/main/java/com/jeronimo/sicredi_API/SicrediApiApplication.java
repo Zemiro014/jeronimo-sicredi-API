79c9485ac8a7d80e4f6072cab3b8caf53d680f82
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-10 14:12:41 -0300

    Primeiro commit

---
+++ src/main/java/com/jeronimo/sicredi_API/SicrediApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeronimo.sicredi_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicrediApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicrediApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeronimo/sicredi_API/SicrediApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeronimo.sicredi_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
