636b66d959aaa88db009510d73ebe5cd6162882e
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-10 20:33:37 -0300

    Criando o instanciador do banco de dados

---
+++ src/main/java/com/jeronimo/sicredi_API/config/DataBase_instantiater.java
@@ -0,0 +1,30 @@
+package com.jeronimo.sicredi_API.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.jeronimo.sicredi_API.domain.Associate;
+import com.jeronimo.sicredi_API.repositories.AssociateRepository;
+
+@Configuration
+public class DataBase_instantiater implements CommandLineRunner {
+
+	@Autowired
+	private AssociateRepository associateRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		associateRepository.deleteAll();
+		
+		Associate maria = new Associate(null, "Maria Brown", "maria@gmail.com");
+		Associate alex = new Associate(null, "Alex Green", "alex@gmail.com");
+		Associate bob = new Associate(null, "Bob Grey", "bob@gmail.com");
+		
+		associateRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
