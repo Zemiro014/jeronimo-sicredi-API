062fbe12dd85b72ac37b5502c0b459459441a466
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-12 16:16:45 -0300

    Refenciando o VotingSession com os seus respectivos Associat e Guideline

--- src/main/java/com/jeronimo/sicredi_API/config/DataBase_instantiater.java
+++ src/main/java/com/jeronimo/sicredi_API/config/DataBase_instantiater.java
@@ -1,5 +1,6 @@
 package com.jeronimo.sicredi_API.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +10,14 @@ import org.springframework.context.annotation.Configuration;
 import com.jeronimo.sicredi_API.domain.Associate;
 import com.jeronimo.sicredi_API.domain.Guideline;
 import com.jeronimo.sicredi_API.domain.Vote;
+import com.jeronimo.sicredi_API.domain.VotingSession;
 import com.jeronimo.sicredi_API.domain.enums.VotingValue;
 import com.jeronimo.sicredi_API.dto.AuthorDTO;
 import com.jeronimo.sicredi_API.dto.VoteForAgendaDTO;
 import com.jeronimo.sicredi_API.repositories.AssociateRepository;
 import com.jeronimo.sicredi_API.repositories.GuidelineRepository;
 import com.jeronimo.sicredi_API.repositories.VoteRepository;
+import com.jeronimo.sicredi_API.repositories.VotingSessionRepository;
 
 @Configuration
 public class DataBase_instantiater implements CommandLineRunner {
@@ -28,12 +31,18 @@ public class DataBase_instantiater implements CommandLineRunner {
 	@Autowired
 	private GuidelineRepository guidelineRepository;
 	
+	@Autowired
+	private VotingSessionRepository votingSessionRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
 		associateRepository.deleteAll();
 		voteRepository.deleteAll();
 		guidelineRepository.deleteAll();
+		votingSessionRepository.deleteAll();
 		
 		Associate maria = new Associate(null, "Maria Brown", "maria@gmail.com");
 		Associate alex = new Associate(null, "Alex Green", "alex@gmail.com");
@@ -54,6 +63,12 @@ public class DataBase_instantiater implements CommandLineRunner {
 		Vote vote6 = new Vote(null, VotingValue.SIM, new VoteForAgendaDTO(guideline2), new AuthorDTO(bob));
 		
 		voteRepository.saveAll(Arrays.asList(vote1, vote2, vote3, vote4, vote5, vote6));
+		
+		VotingSession session1 = new VotingSession(null, sdf.parse("10/03/2021 09:30:00"), sdf.parse("10/03/2021 11:00:00"), guideline1);
+		VotingSession session2 = new VotingSession(null, sdf.parse("10/03/2021 14:00:00"), sdf.parse("10/03/2021 16:00:00"), guideline2);
+		session1.getAssociates().addAll(Arrays.asList(maria, alex, bob));
+		session2.getAssociates().addAll(Arrays.asList(maria, alex, bob));
+		votingSessionRepository.saveAll(Arrays.asList(session1, session2));
 		
 		maria.getVotes().addAll(Arrays.asList(vote1, vote2));
 		alex.getVotes().addAll(Arrays.asList(vote3, vote4));
---
+++ src/main/java/com/jeronimo/sicredi_API/repositories/VotingSessionRepository.java
@@ -0,0 +1,11 @@
+package com.jeronimo.sicredi_API.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jeronimo.sicredi_API.domain.VotingSession;
+
+@Repository
+public interface VotingSessionRepository extends MongoRepository<VotingSession, String>{
+
+}
